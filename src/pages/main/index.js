6c442b495bfb03d9a552c768b0a46131df2b8227
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-05-14 08:55:32 -0300

    criada pagina

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -6,6 +6,8 @@ import './styles.css';
 export default class Main extends Component {
     state = {
         products: [],
+        productInfo: {},
+        page: 1,
     }
     
     // Executar uma ação assim que o componente é criado em tela
@@ -13,14 +15,36 @@ export default class Main extends Component {
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-        const response = await api.get('/products');
-        console.log(response.data.docs);
-        this.setState({products: response.data.docs});
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
+        
+        const { docs, ...productInfo } = response.data;
+
+        this.setState({products: docs, productInfo, page });
     };
 
+    prevPage = () => {
+        const { page } = this.state;
+
+        if(page === 1) return;
+
+        const pageNumber = page - 1;
+
+        this.loadProducts(pageNumber);
+    }
+
+    nextPage = () => {
+        const { page, productInfo } = this.state;
+
+        if(page === productInfo.pages) return;
+
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
+    }
+    
     render() {
-        const { products } = this.state;
+        const { products, page, productInfo } = this.state;
 
         return (
             <div className="product-list">
@@ -32,6 +56,10 @@ export default class Main extends Component {
                     </article>
 
                 ))}
+                <div className="actions">
+                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                    <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próximo</button>
+                </div>
             </div>
         )
     }
